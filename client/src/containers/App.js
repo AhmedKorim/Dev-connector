26da5f53ef6dcf13d862ad4ae9e9bdac353264ef
ahmedkorim <ahmedkorrim@gmail.com>
2018-10-17 08:00:59 +0200

    jquery font awsome and bootstrap

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+
 import './App.css';
 
 class App extends Component {
+
+
     render() {
         return (
-            <h1>react</h1>
+        <div>
+            hi
+        </div>
         );
     }
 }
