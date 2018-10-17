f30cdd7a958aefbe79b4048c7e7581200070fb02
ahmedkorim <ahmedkorrim@gmail.com>
2018-10-17 08:37:39 +0200

    landing page and register

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,17 +1,41 @@
-import React, {Component} from 'react';
-
+import React, {Component, Fragment} from 'react';
+import Login from "../components/Auth/Login";
+import Register from "../components/Auth/Register";
+import Footer from "../components/layout/Footer";
+import Landing from "../components/layout/Landing";
+import Navbar from "../components/layout/Navbar";
+import {Route} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-
-
     render() {
         return (
-        <div>
-            hi
-        </div>
-        );
-    }
+            <Fragment>
+                <Navbar/>
+                <main>
+                    <Route
+                        path="/"
+                        exact
+                        component={Landing}
+                    />
+                    <div className="container">
+                        <Route
+                            path="/register"
+                            exact
+                            component={Register}
+                        />
+                        <Route
+                            path="/login"
+                            exact
+                            component={Login}
+                        />
+                    </div>
+                </main>
+                <Footer/>
+            </Fragment>
+
+        )
+    };
 }
 
 export default App;
